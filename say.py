09f56fa6b189c52bc10a00abd11fde288bf249a0
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2024-07-27 00:09:29 -0700

    better parallelization

--- say.py
+++ say.py
@@ -1,5 +1,7 @@
-import os
 import argparse
+import os
+import random
+from multiprocessing import Process
 from typing import TYPE_CHECKING
 
 
@@ -34,84 +36,137 @@ def split_text(text: str, chunk_count: int) -> list[str]:
 
 def tts_chunk(
     rank: int,
-    texts: list[str],
-    queues: "list[Queue]",
+    input_queues: "list[Queue]",
+    output_queues: "list[Queue]",
     emotion: str,
     speaker: str | None = None,
     speaker_wav: str | None = None
 ):
     import torch
 
-    torch.cuda.set_device(rank)
-    queue = queues[rank]
-    text = texts[rank]
+    input_queue = input_queues[rank]
+    output_queue = output_queues[rank]
 
     from TTS.api import TTS
     from TTS.utils.synthesizer import Synthesizer
 
     # print('Initializing Model')
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=torch.cuda.is_available())
+    device = f"cuda:{rank}" if torch.cuda.is_available() else "cpu"
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-    # print('Generating Voice')
-    if speaker_wav is not None:
-        queue.put(tts.tts(
-            text=text,
-            language="en",
-            emotion=emotion,
-            speaker_wav=speaker_wav,
-        ))
-    else:
-        queue.put(tts.tts(
-            text=text,
-            language="en",
-            emotion=emotion,
-            speaker=speaker or "Tanja Adelina",
-        ))
     synthesizer = tts.synthesizer
     assert isinstance(synthesizer, Synthesizer)
-    queue.put(synthesizer.output_sample_rate)
-    return
 
+    while True:
+        input_values = input_queue.get()
+        if input_values is None:
+            break
 
-def tts_to_file(text: str, output_path: str, emotion: str, speaker: str | None = None, speaker_wav: str | None = None):
-    text = process_text(text)
+        text, output_path, idx = input_values
 
-    import torch
-    import torch.multiprocessing as mp
-    import torch.multiprocessing.spawn
-    from TTS.utils.audio.numpy_transforms import save_wav
-
-    if torch.cuda.is_available():
-        device_count = torch.cuda.device_count()
-    else:
-        device_count = 1
-    torch.cuda.empty_cache()
-
-    qs = [mp.Queue(2) for _ in range(device_count)]
-    texts = split_text(text, device_count)
-    context = torch.multiprocessing.spawn(
-        tts_chunk,
-        args=(texts, qs, emotion, speaker, speaker_wav),
-        nprocs=device_count,
-        join=False
-    )
-
-    wav: list[int] = []
-    sample_rate = None
-    for q in qs:
-        w, _sample_rate = q.get(), q.get()
-        wav += list(w) + [0] * 10000
-
-        if sample_rate is None:
-            sample_rate = _sample_rate
+        # print('Generating Voice')
+        if speaker_wav is not None:
+            wav = tts.tts(
+                text=text,
+                language="en",
+                emotion=emotion,
+                speaker_wav=speaker_wav,
+            )
         else:
-            assert sample_rate == _sample_rate, (sample_rate, _sample_rate)
+            wav = tts.tts(
+                text=text,
+                language="en",
+                emotion=emotion,
+                speaker=speaker or "Tanja Adelina",
+            )
+        output_queue.put((wav, synthesizer.output_sample_rate, output_path, idx))
     
-    assert context is not None
-    context.join(1)
+    output_queue.put(None)
 
-    assert isinstance(sample_rate, int)
-    save_wav(wav=np.array(wav), path=output_path, sample_rate=sample_rate)
+
+def waves_to_file(output_queues: "list[Queue]"):
+    while True:
+        waves: list[tuple[int, list[int]]] = []
+        sample_rate = None
+        output_path = None
+        output_values = [q.get() for q in output_queues]
+
+        if all(v is None for v in output_values):
+            return
+
+        assert all(v is not None for v in output_values)
+        for w, _sample_rate, _output_path, _idx in output_values:
+            if output_path is None:
+                output_path = _output_path
+            assert _output_path == output_path, (output_path, _output_path)
+
+            waves.append((_idx, w))
+
+            if sample_rate is None:
+                sample_rate = _sample_rate
+            assert sample_rate == _sample_rate, (sample_rate, _sample_rate)
+        
+        wav: list[int] = []
+        for _, w in sorted(waves):
+            wav += list(w) + [0] * 10000
+        
+        from TTS.utils.audio.numpy_transforms import save_wav
+        assert isinstance(sample_rate, int)
+        assert isinstance(output_path, str)
+        save_wav(wav=np.array(wav), path=output_path, sample_rate=sample_rate)
+
+
+class TTSToFile:
+    def __init__(
+        self,
+        emotion: str | None = None,
+        speaker: str | None = None,
+        speaker_wav: str | None = None
+    ):
+        import torch
+        import torch.multiprocessing as mp
+
+        if torch.cuda.is_available():
+            self.device_count = torch.cuda.device_count()
+        else:
+            self.device_count = 1
+        torch.cuda.empty_cache()
+
+        self.output_queues = [mp.Queue() for _ in range(self.device_count)]
+        self.input_queues = [mp.Queue() for _ in range(self.device_count)]
+        context = mp.spawn(
+            tts_chunk,
+            args=(self.input_queues, self.output_queues, emotion, speaker, speaker_wav),
+            nprocs=self.device_count,
+            join=False
+        )
+        assert context is not None
+        self.context = context
+        self.writer = Process(target=waves_to_file, args=(self.output_queues,))
+        self.writer.start()
+    
+    def tts_to_file(self, text: str, output_path: str):
+        texts = split_text(text, self.device_count)
+        for idx, (text, queue) in enumerate(zip(
+            texts,
+            random.sample(self.input_queues, len(self.input_queues))
+        )):
+            queue.put((text, output_path, idx))
+    
+    def close(self):
+        for q in self.input_queues:
+            q.put(None)
+        self.context.join(1)
+
+        for q in self.input_queues:
+            q.close()
+
+        self.writer.join()
+        self.writer.kill()
+        self.writer.terminate()
+        
+        for q in self.output_queues:
+            q.close()
 
 
 def main():
@@ -147,13 +202,17 @@ def main():
 
 
     torch.multiprocessing.set_start_method('spawn')
+    tts = TTSToFile(args.emotion, args.speaker)
     if args.dir is None:
-        tts_to_file(args.text, args.output, args.emotion, args.speaker)
+        tts.tts_to_file(args.text, args.output)
     else:
         for filename in sorted(os.listdir(args.dir)):
-            path = os.path.join(args.dir, filename)
-            with open(path, 'r') as f:
-                tts_to_file(f.read(), path + '.wav', args.emotion, args.speaker)
+            if not filename.endswith('.wav'):
+                path = os.path.join(args.dir, filename)
+                with open(path, 'r') as f:
+                    tts.tts_to_file(f.read(), path + '.wav')
+    
+    tts.close()
 
 
 if __name__ == '__main__':
